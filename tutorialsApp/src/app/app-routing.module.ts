c8fc860139e11804fa9e56a1f9dc5ef0134c0b4d
merciof <mercio.andrade@ufrpe.br>
2021-06-09 10:39:08 -0300

    routes config

--- tutorialsApp/src/app/app-routing.module.ts
+++ tutorialsApp/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+import { TutorialDetalhesComponent } from './components/tutorial-detalhes/tutorial-detalhes.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'tutorials', pathMatch: 'full' },
+  { path: 'tutorials', component: AddTutorialComponent },
+  { path: 'tutorials/:id', component: TutorialDetalhesComponent },
+  { path: 'add', component: AddTutorialComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
