3a7dc7155c08c86534138ff5c732e1865d833444
merciof <mercio.andrade@ufrpe.br>
2021-06-09 10:37:07 -0300

    add imports FormsModule, HttpClientModule

--- tutorialsApp/src/app/app.module.ts
+++ tutorialsApp/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
 import { TutorialDetalhesComponent } from './components/tutorial-detalhes/tutorial-detalhes.component';
 import { TutorialListComponent } from './components/tutorial-list/tutorial-list.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +19,9 @@ import { TutorialListComponent } from './components/tutorial-list/tutorial-list.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
