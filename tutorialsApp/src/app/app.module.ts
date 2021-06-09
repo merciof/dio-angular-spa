fd49b3f0f99f7295648b03a6b458549a25cfea1b
merciof <mercio.andrade@ufrpe.br>
2021-06-09 10:32:43 -0300

    app module imports update

--- tutorialsApp/src/app/app.module.ts
+++ tutorialsApp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+import { TutorialDetalhesComponent } from './components/tutorial-detalhes/tutorial-detalhes.component';
+import { TutorialListComponent } from './components/tutorial-list/tutorial-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTutorialComponent,
+    TutorialDetalhesComponent,
+    TutorialListComponent
   ],
   imports: [
     BrowserModule,
